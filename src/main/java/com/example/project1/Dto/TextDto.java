350ee9ef831edb6fdde953683ba2da0f04384abb
ham8962 <zzing199842@gmial.com>
2023-10-30 20:25:33 +0900

    TextDto

---
+++ src/main/java/com/example/project1/Dto/TextDto.java
@@ -0,0 +1,33 @@
+package com.example.project1.Dto;
+
+public class TextDto {
+    private String textID;
+    private String rpm;
+    private String speed;
+
+    public void setTextID(String textID) {
+        this.textID = textID;
+    }
+
+    public void setRPM(String RPM) {
+        this.rpm = RPM;
+    }
+
+    public void setSpeed(String Speed) {
+        this.speed = Speed;
+    }
+
+    public String getTextID() {
+        return this.textID;
+    }
+
+    public String getRPM() {
+        return this.rpm;
+    }
+
+    public String getSpeed() {
+        return this.speed;
+    }
+}
+
+
